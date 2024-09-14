cf86a67806e406e72fe01cd385e20128b18a4d0d
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-13 22:13:27 -0300

    Primer commit
    Configuraciones automáticas iniciales

---
+++ src/main/java/org/spdgroup/bigbitebackend/BigBiteBackendApplication.java
@@ -0,0 +1,13 @@
+package org.spdgroup.bigbitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spdgroup/bigbitebackend/BigBiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spdgroup.bigbitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
