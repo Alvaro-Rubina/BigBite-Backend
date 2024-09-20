72092df79e6632539182775c2553874814740cea
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-20 14:09:00 -0300

    Creado UsuarioController

--- src/main/java/org/spdgroup/bigbitebackend/controllers/UsuarioController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/UsuarioController.java
@@ -3,18 +3,27 @@ package org.spdgroup.bigbitebackend.controllers;
 import org.spdgroup.bigbitebackend.model.entities.Usuario;
 import org.spdgroup.bigbitebackend.services.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller("/usuario")
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private IUsuarioService usuarioService;
 
-    @PostMapping("/save")
-    public void saveUsuario(@RequestBody Usuario usuario){
+    @PostMapping("/nuevo")
+    public ResponseEntity<Map<String, String>> saveUsuario(@RequestBody Usuario usuario){
         usuarioService.saveUsuario(usuario);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Usuario guardado exitosamente");
+        return ResponseEntity.ok(response);
     }
+
 }
