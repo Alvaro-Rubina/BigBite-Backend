9463f219608756924083672b0089fc05c26d01c7
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-20 22:36:35 -0300

    Creada clase UsuarioDTO

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package org.spdgroup.bigbitebackend.model.dtos;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UsuarioDTO {
+
+    //
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String password;
+}
--- src/main/java/org/spdgroup/bigbitebackend/model/entities/Usuario.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/entities/Usuario.java
@@ -5,8 +5,8 @@ import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Getter @Setter
+@Builder
 @ToString
 @Entity
 public class Usuario {
