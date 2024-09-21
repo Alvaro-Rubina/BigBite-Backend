c5762913dc870c99be67ed29050ea5910c22e19a
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-20 22:49:41 -0300

    Actualizado endpoint para registrar usuario

--- src/main/java/org/spdgroup/bigbitebackend/controllers/UsuarioController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/UsuarioController.java
@@ -1,14 +1,12 @@
 package org.spdgroup.bigbitebackend.controllers;
 
-import org.spdgroup.bigbitebackend.model.entities.Usuario;
+import org.spdgroup.bigbitebackend.model.dtos.UsuarioDTO;
 import org.spdgroup.bigbitebackend.services.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -18,12 +16,14 @@ public class UsuarioController {
     @Autowired
     private IUsuarioService usuarioService;
 
-    @PostMapping("/nuevo")
-    public ResponseEntity<Map<String, String>> saveUsuario(@RequestBody Usuario usuario){
-        usuarioService.saveUsuario(usuario);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Usuario guardado exitosamente");
-        return ResponseEntity.ok(response);
+    @PostMapping("/registrar")
+    public ResponseEntity<String> registrar(@RequestBody UsuarioDTO usuario){
+        try {
+            usuarioService.registrar(usuario);
+            return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
