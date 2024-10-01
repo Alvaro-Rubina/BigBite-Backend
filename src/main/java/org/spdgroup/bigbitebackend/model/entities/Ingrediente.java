8b3a229f349fe4fe1effbbffe41fae269c08250a
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-01 20:25:40 -0300

    Creada entidad Ingrediente

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/entities/Ingrediente.java
@@ -0,0 +1,26 @@
+package org.spdgroup.bigbitebackend.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.spdgroup.bigbitebackend.model.enums.UnidadMedida;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+@ToString
+@Entity
+public class Ingrediente {
+
+    //
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private int cantidad;
+    private UnidadMedida unidadMedida;
+
+}
