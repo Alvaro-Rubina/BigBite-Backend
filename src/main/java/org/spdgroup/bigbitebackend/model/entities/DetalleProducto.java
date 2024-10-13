317d7a93106a2996371b10a131147a0c2a023e48
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-13 02:02:49 -0300

    Creada entidad DetalleProducto
    Creada entidad pero todavía no está terminada, hay que ver como almacenar los distintos productos (Producto es una clase abstracta que no se persiste, por ahora)

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/entities/DetalleProducto.java
@@ -0,0 +1,22 @@
+package org.spdgroup.bigbitebackend.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+@Entity
+public class DetalleProducto {
+
+    //
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int cantidad;
+
+    // TODO: VER COMO LOGRAR ESTO TENIENDO EN CUENTA LA HERENCIA etc ma{ñana veo
+    /*@ManyToOne
+    private Producto producto;*/
+}
