96ca5c92672487736013efc4d5593fd9de1f6e92
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-13 00:05:20 -0300

    Creada entidad Bebida

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/entities/Bebida.java
@@ -0,0 +1,25 @@
+package org.spdgroup.bigbitebackend.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.spdgroup.bigbitebackend.model.enums.Marca;
+import org.spdgroup.bigbitebackend.model.enums.Tamanio;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Bebida extends Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Tamanio tamanio;
+    private Marca marca;
+    // TODO: AGREGAR MAS ATRIBUTOS PARA BEBIDAS
+}
