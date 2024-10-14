4c0d8d4897694f3072f87475da269473970f549e
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-14 18:39:00 -0300

    Creado HamburguesaService

---
+++ src/main/java/org/spdgroup/bigbitebackend/services/HamburguesaService.java
@@ -0,0 +1,35 @@
+package org.spdgroup.bigbitebackend.services;
+
+import org.spdgroup.bigbitebackend.model.dtos.request.HamburguesaDTO;
+import org.spdgroup.bigbitebackend.model.entities.Hamburguesa;
+import org.spdgroup.bigbitebackend.repositories.IHamburguesaRepository;
+import org.spdgroup.bigbitebackend.utils.mapper.HamburguesaMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class HamburguesaService {
+
+    @Autowired
+    IHamburguesaRepository hamburguesaRepo;
+
+    @Autowired
+    HamburguesaMapper hamburguesaMapper;
+
+    @Autowired
+    private GoogleCloudStorageService storageService;
+
+    public void registrarHamburguesa(HamburguesaDTO hamburguesaDTO, MultipartFile imagen) throws IOException {
+
+        // Se guarda la imagen de la hamburguesa
+        String imagenHamburguesaUrl = storageService.uploadFile(imagen);
+
+        hamburguesaDTO.setUrlImage(imagenHamburguesaUrl);
+        Hamburguesa hamburguesa = hamburguesaMapper.hamburguesaDTOToHamburguesa(hamburguesaDTO);
+
+        hamburguesaRepo.save(hamburguesa);
+    }
+}
