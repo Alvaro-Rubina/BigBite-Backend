fec6bebe72bbc35810a1e73dee33b888f9e92f4f
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-14 00:20:05 -0300

    Creado PapasFritasRequest

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/request/PapasFritasRequest.java
@@ -0,0 +1,12 @@
+package org.spdgroup.bigbitebackend.model.dtos.request;
+
+import org.spdgroup.bigbitebackend.model.enums.Tamanio;
+
+public record PapasFritasRequest(String nombre,
+                                 String descripcion,
+                                 double precioCosto,
+                                 double precioVenta,
+                                 int stockActual,
+                                 int tiempoPreparacion,
+                                 Tamanio tamanio) {
+}
