d6aad1653ad581cb9860090e13436d172cd648e6
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-14 00:35:37 -0300

    Actualizados atributos de PapasFritasRequest

--- src/main/java/org/spdgroup/bigbitebackend/model/dtos/request/PapasFritasRequest.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/request/PapasFritasRequest.java
@@ -2,11 +2,14 @@ package org.spdgroup.bigbitebackend.model.dtos.request;
 
 import org.spdgroup.bigbitebackend.model.enums.Tamanio;
 
+import java.util.List;
+
 public record PapasFritasRequest(String nombre,
                                  String descripcion,
                                  double precioCosto,
                                  double precioVenta,
                                  int stockActual,
                                  int tiempoPreparacion,
-                                 Tamanio tamanio) {
+                                 Tamanio tamanio,
+                                 List<String> ingredientes) {
 }
