a0b571476251590db5b54d9ef0617f47dbfbf8ce
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-14 00:36:45 -0300

    Creado HamburguesaRequest

---
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/request/HamburguesaRequest.java
@@ -0,0 +1,12 @@
+package org.spdgroup.bigbitebackend.model.dtos.request;
+
+import java.util.List;
+
+public record HamburguesaRequest (String nombre,
+                                 String descripcion,
+                                 double precioCosto,
+                                 double precioVenta,
+                                 int stockActual,
+                                 int tiempoPreparacion,
+                                 List<String> ingredientes){
+}
