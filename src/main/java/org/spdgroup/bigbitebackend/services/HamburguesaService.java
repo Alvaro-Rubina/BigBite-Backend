f107e861d25f5ded6499ee59c7d855e330074ae6
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-15 16:30:03 -0300

    Actualizado editarHamburguesa

--- src/main/java/org/spdgroup/bigbitebackend/services/HamburguesaService.java
+++ src/main/java/org/spdgroup/bigbitebackend/services/HamburguesaService.java
@@ -3,12 +3,14 @@ package org.spdgroup.bigbitebackend.services;
 import org.spdgroup.bigbitebackend.model.dtos.HamburguesaDTO;
 import org.spdgroup.bigbitebackend.model.entities.Hamburguesa;
 import org.spdgroup.bigbitebackend.repositories.IHamburguesaRepository;
+import org.spdgroup.bigbitebackend.utils.exception.ProductNotFoundException;
 import org.spdgroup.bigbitebackend.utils.mapper.HamburguesaMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class HamburguesaService {
@@ -25,11 +27,32 @@ public class HamburguesaService {
     public void registrarHamburguesa(HamburguesaDTO hamburguesaDTO, MultipartFile imagen) throws IOException {
 
         // Se guarda la imagen de la hamburguesa
-        String imagenHamburguesaUrl = storageService.uploadFile(imagen);
+        String imagenUrl = storageService.uploadFile(imagen);
 
-        hamburguesaDTO.setUrlImage(imagenHamburguesaUrl);
+        hamburguesaDTO.setUrlImage(imagenUrl);
         Hamburguesa hamburguesa = hamburguesaMapper.hamburguesaDTOToHamburguesa(hamburguesaDTO);
 
         hamburguesaRepo.save(hamburguesa);
+    }
+
+    public List<Hamburguesa> obtenerHamburguesas(){
+        return hamburguesaRepo.findAll();
+    }
+
+    public void editarHamburguesa(HamburguesaDTO hamburguesaDTO, MultipartFile imagen, Long id) throws IOException {
+
+        if (!hamburguesaRepo.existsById(id)) {
+            throw new ProductNotFoundException("Hamburguesa no encontrada");
+        }
+
+        // Se guarda la imagen de la hamburguesa
+        String imagenUrl = storageService.uploadFile(imagen);
+        hamburguesaDTO.setUrlImage(imagenUrl);
+
+        Hamburguesa hamburguesa = hamburguesaMapper.hamburguesaDTOToHamburguesa(hamburguesaDTO);
+        hamburguesa.setId(id);
+
+        hamburguesaRepo.save(hamburguesa);
+
     }
 }
