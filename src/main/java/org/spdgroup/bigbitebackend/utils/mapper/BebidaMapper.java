5fa8b305a16ac8a3fed41b2eec7d7099909359e4
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-17 00:50:05 -0300

    Creado BebidaMapper

---
+++ src/main/java/org/spdgroup/bigbitebackend/utils/mapper/BebidaMapper.java
@@ -0,0 +1,11 @@
+package org.spdgroup.bigbitebackend.utils.mapper;
+
+import org.mapstruct.Mapper;
+import org.spdgroup.bigbitebackend.model.dtos.BebidaDTO;
+import org.spdgroup.bigbitebackend.model.entities.Bebida;
+
+@Mapper(componentModel = "spring", uses = BebidaMapper.class)
+public interface BebidaMapper {
+
+    Bebida bebidaDTOToBebida(BebidaDTO bebidaDTO);
+}
