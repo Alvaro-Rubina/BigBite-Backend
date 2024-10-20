9cfb20f5a8c1f44886e02b94ff3ce7516fe15fed
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-20 12:52:06 -0300

    Creado InsumoMapper

---
+++ src/main/java/org/spdgroup/bigbitebackend/utils/mapper/InsumoMapper.java
@@ -0,0 +1,11 @@
+package org.spdgroup.bigbitebackend.utils.mapper;
+
+import org.mapstruct.Mapper;
+import org.spdgroup.bigbitebackend.model.dtos.InsumoDTO;
+import org.spdgroup.bigbitebackend.model.entities.Insumo;
+
+@Mapper(componentModel = "spring", uses = InsumoMapper.class)
+public interface InsumoMapper {
+
+    Insumo toEntity (InsumoDTO insumoDTO);
+}
