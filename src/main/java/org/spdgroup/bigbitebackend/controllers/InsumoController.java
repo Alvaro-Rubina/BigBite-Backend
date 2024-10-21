2b559a99574d3e8aaf23863c69d716bc46acb6d7
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-20 22:13:57 -0300

    Creado InsumoController

---
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/InsumoController.java
@@ -0,0 +1,45 @@
+package org.spdgroup.bigbitebackend.controllers;
+
+import org.spdgroup.bigbitebackend.model.dtos.InsumoDTO;
+import org.spdgroup.bigbitebackend.model.entities.Insumo;
+import org.spdgroup.bigbitebackend.services.InsumoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/insumos")
+public class InsumoController {
+
+    @Autowired
+    private InsumoService insumoService;
+
+    @GetMapping
+    public List<Insumo> obtenerInsumos() {
+        return insumoService.obtenerInsumos();
+    }
+
+    @PostMapping("/agregar")
+    public ResponseEntity<String> registrarInsumo(@RequestBody InsumoDTO insumoDTO) {
+        try {
+            insumoService.registrarInsumo(insumoDTO);
+            return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<String> editarInsumo(@RequestBody InsumoDTO insumoDTO, @PathVariable Long id) {
+        try {
+            insumoService.editarInsumo(insumoDTO, id);
+            return new ResponseEntity<>("Edición exitosa", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
