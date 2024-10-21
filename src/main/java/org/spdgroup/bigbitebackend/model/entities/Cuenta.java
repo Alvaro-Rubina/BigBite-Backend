139464df8913c1eb5ccd582f7e1be420bde45039
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-21 00:36:20 -0300

    Actualizada entidad Cuetna

--- src/main/java/org/spdgroup/bigbitebackend/model/entities/Cuenta.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/entities/Cuenta.java
@@ -10,8 +10,19 @@ import lombok.experimental.SuperBuilder;
 @Builder
 @Entity
 public class Cuenta {
+
+    //
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String codigo;
     private String nombre;
     private String naturaleza;
+
+    public Cuenta(String codigo, String nombre, String naturaleza) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.naturaleza = naturaleza;
+    }
 }
