0b81d037eb37468363fa17e3369967fa503d9399
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-21 17:37:45 -0300

    creada excepcion PedidoNotFoundException

---
+++ src/main/java/org/spdgroup/bigbitebackend/utils/exception/PedidoNotFoundException.java
@@ -0,0 +1,7 @@
+package org.spdgroup.bigbitebackend.utils.exception;
+
+public class PedidoNotFoundException extends RuntimeException {
+    public PedidoNotFoundException(String message) {
+        super(message);
+    }
+}
