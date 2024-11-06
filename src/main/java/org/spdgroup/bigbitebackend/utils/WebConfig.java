cf7268e6763c3d73389bd9aa96f9036f8e6530f3
Alvaro Rubina <alvarorubina132@gmail.com>
2024-11-06 00:33:41 -0300

    solucion error de sintaxis

--- src/main/java/org/spdgroup/bigbitebackend/utils/WebConfig.java
+++ src/main/java/org/spdgroup/bigbitebackend/utils/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
