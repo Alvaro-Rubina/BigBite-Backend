b9d2f24491d3075f4e9b917dd70c958797eaf0d3
Alvaro Rubina <alvarorubina132@gmail.com>
2024-11-05 22:58:07 -0300

    Actualización CORS

--- src/main/java/org/spdgroup/bigbitebackend/controllers/InsumoController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/InsumoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/insumos")
 public class InsumoController {
 
