f54f858dc155bc1409834d386ed557d319619896
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-28 23:51:26 -0300

    Agregado swagger al proyecto, configuraciones iniciales

---
+++ src/main/java/org/spdgroup/bigbitebackend/utils/swagger/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.spdgroup.bigbitebackend.utils.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Big Bite API")
+                        .description("API desarrollada con Spring Boot conectada a una base de datos MySQL, " +
+                                "que al ser consumida por un cliente, en este caso un e-commerce de hamburguesas, " +
+                                "Big Bite, permite la gestion y persistencia de productos, pedidos y asientos contables.\n\n" +
+                                "Repositorios:\n" +
+                                "- Backend: [https://github.com/Alvaro-Rubina/BigBite-Backend](https://github.com/Alvaro-Rubina/BigBite-Backend)\n" +
+                                "- Frontend: [https://github.com/candelabp/Big-bite](https://github.com/candelabp/Big-bite)")
+                );
+    }
+}
