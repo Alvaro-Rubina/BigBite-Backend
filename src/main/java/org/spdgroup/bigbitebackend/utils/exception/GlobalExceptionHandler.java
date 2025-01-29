c9333dc1f29b62648e8bc6fe79a16850c0e77f57
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-29 16:45:13 -0300

    Añadida excepcion para validaciones al exception handler

--- src/main/java/org/spdgroup/bigbitebackend/utils/exception/GlobalExceptionHandler.java
+++ src/main/java/org/spdgroup/bigbitebackend/utils/exception/GlobalExceptionHandler.java
@@ -2,14 +2,25 @@ package org.spdgroup.bigbitebackend.utils.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
-        return new ResponseEntity<>(("No existe un producto llamado " + ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>((ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errors = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(". ")) + ".";
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
