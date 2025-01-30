ada5ac5e4fc0d5e0b06a264ac86e6abc15e961d9
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-30 00:53:55 -0300

    Documentados CuentaController y CuentaDTO

--- src/main/java/org/spdgroup/bigbitebackend/controllers/CuentaController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/CuentaController.java
@@ -1,4 +1,11 @@
 package org.spdgroup.bigbitebackend.controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.spdgroup.bigbitebackend.model.dtos.CuentaDTO;
 import org.spdgroup.bigbitebackend.model.entities.Cuenta;
 import org.spdgroup.bigbitebackend.services.CuentaService;
@@ -10,25 +17,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Cuentas", description = "Metodos para registrar y obtener cuentas")
 @RequestMapping("/api/cuentas")
 public class CuentaController {
 
     @Autowired
     private CuentaService cuentaService;
 
-    @GetMapping
+    @Operation(summary = "Devuelve todas las cuentas de la BBDD")
+    @GetMapping @ResponseBody
     public List<Cuenta> obtenerCuentas() {
         return cuentaService.obtenerCuentas();
     }
 
+    @Operation(summary = "Registra una nueva cuenta en la BBDD")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cuenta registrada", content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CuentaDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Error al registrar la cuenta", content = @Content)
+    })
     @PostMapping("/registrar")
-    public ResponseEntity<String> registrarCuenta(@RequestPart CuentaDTO cuentaDTO) {
-        try {
-            cuentaService.registrarCuenta(cuentaDTO);
-            return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> registrarCuenta(@RequestBody @Valid CuentaDTO cuentaDTO) {
+        cuentaService.registrarCuenta(cuentaDTO);
+        return new ResponseEntity<>("Cuenta registrada exitosamente", HttpStatus.CREATED);
     }
 
 }
--- src/main/java/org/spdgroup/bigbitebackend/model/dtos/CuentaDTO.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/CuentaDTO.java
@@ -1,8 +1,20 @@
 package org.spdgroup.bigbitebackend.model.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
 public class CuentaDTO {
+
+    @Schema(example = "2.1.1.01") @NotBlank(message = "El codigo es obligatorio")
     private String codigo;
+    @Schema(example = "Proveedores") @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+    @Schema(example = "Pasivo") @NotBlank(message = "El tipo de cuenta es obligatorio")
     private String tipoCuenta;
 
 }
