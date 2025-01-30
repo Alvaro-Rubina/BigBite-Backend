6440abf36b3c8882f79d54cc34c243d87800e3c1
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-30 17:23:27 -0300

    Documentados BiteBoxController y BiteBoxDTO

--- src/main/java/org/spdgroup/bigbitebackend/controllers/BiteBoxController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/BiteBoxController.java
@@ -1,5 +1,13 @@
 package org.spdgroup.bigbitebackend.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.spdgroup.bigbitebackend.model.dtos.BiteBoxDTO;
 import org.spdgroup.bigbitebackend.model.entities.BiteBox;
 import org.spdgroup.bigbitebackend.services.BiteBoxService;
@@ -7,45 +15,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "BiteBox", description = "Metodos para registrar, obtener y editar Bite Boxes")
 @RequestMapping("/api/bite-box")
 public class BiteBoxController {
 
     @Autowired
     private BiteBoxService biteBoxService;
 
+    @Operation(summary = "Devuelve todas las Bite Boxes de la BBDD")
     @GetMapping
     public List<BiteBox> obtenerBiteBoxes() {
         return biteBoxService.obtenerBiteBoxes();
     }
 
+    @Operation(summary = "Busca una Bite Box por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bite Box encontrada", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = BiteBoxDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Bite Box no encontrada", content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<?> obtenerBiteBoxPorId(@PathVariable Long id) {
+    public ResponseEntity<?> obtenerBiteBoxPorId(@Parameter(description = "ID de la Bite Box", example = "1") @PathVariable Long id) {
         BiteBox biteBox = biteBoxService.obtenerBiteBoxPorId(id);
-        return ResponseEntity.ok(biteBox);
+        return ResponseEntity.status(HttpStatus.OK).body(biteBox);
     }
 
+    @Operation(summary = "Registra una nueva Bite Box en la BBDD")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "201", description = "Bite Box registrada", content = {
+                    @Content(mediaType = "application/json",
+                            schema =  @Schema(implementation = BiteBoxDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Bebida o Hamburguesa con el ID ingresado no encontrada", content = @Content)
+    })
     @PostMapping("/registrar")
-    public ResponseEntity<String> registrarBiteBox(@RequestBody BiteBoxDTO biteBoxDTO) {
-        try {
-            biteBoxService.registrarBiteBox(biteBoxDTO);
-            return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> registrarBiteBox(@RequestBody @Valid BiteBoxDTO biteBoxDTO) {
+        biteBoxService.registrarBiteBox(biteBoxDTO);
+        return ResponseEntity.status(HttpStatus.OK).body("Registro exitoso");
     }
 
+    @Operation(summary = "Edita una Bite Box")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bite Box editada", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = BiteBoxDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Bite Box no encontrada", content = @Content)
+    })
     @PutMapping("/editar/{id}")
-    public ResponseEntity<String> editarBiteBox(@RequestBody BiteBoxDTO biteBoxDTO, @PathVariable Long id) {
-        try {
-            biteBoxService.editarBiteBox(biteBoxDTO, id);
-            return new ResponseEntity<>("Edición exitosa", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> editarBiteBox(@RequestBody @Valid BiteBoxDTO biteBoxDTO,
+                                                @Parameter(description = "ID de la Bite Box", example = "1") @PathVariable Long id) {
+        biteBoxService.editarBiteBox(biteBoxDTO, id);
+        return ResponseEntity.status(HttpStatus.OK).body("Edicion exitosa");
     }
 }
--- src/main/java/org/spdgroup/bigbitebackend/model/dtos/BiteBoxDTO.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/BiteBoxDTO.java
@@ -1,5 +1,9 @@
 package org.spdgroup.bigbitebackend.model.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,14 +12,39 @@ import lombok.*;
 @Getter @Setter
 public class BiteBoxDTO {
 
+    //
+    @Schema(example = "Bite Box") @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+
+    @Schema(example = "Bite Box preparada especialmente para niños!") @NotBlank(message = "La descripcion es obligatoria")
     private String descripcion;
-    private double precio;
-    private double precioCombo;
-    private int stock;
+
+    @Schema(example = "4500", description = "(Precio de costo)") @NotNull(message = "El precio de costo (precio) es obligatorio")
+    @Min(value = 0, message = "El valor minimo del precio de costo (precio) es 0")
+    private Double precio;
+
+    @Schema(example = "8000", description = "(Precio de venta)") @NotNull(message = "El precio de venta (precioCombo) es obligatorio")
+    @Min(value = 0, message = "El valor minimo del precio de venta (precioCombo) es 0")
+    private Double precioCombo;
+
+    @Schema(example = "150") @NotNull(message = "El stock es obligatorio")
+    @Min(value = 0, message = "El valor minimo del stock es 0")
+    private Integer stock;
+
+    @Schema(example = "true", description = "Por defecto es false, si no se especifica en el JSON, tomará dicho valor")
     private boolean disponible;
-    private boolean contieneJuguete;
+
+    @Schema(example = "https://goo.su/6UySId")
     private String urlImagen;
-    private int hamburguesa;
-    private int bebida;
+
+    @Schema(example = "true", description = "Por defecto es false, si no se especifica en el JSON, tomará dicho valor")
+    private boolean contieneJuguete;
+
+    @Schema(example = "2") @NotNull(message = "El ID de la hamburguesa es obligatorio")
+    @Min(value = 1, message = "El valor minimo para el ID es 1")
+    private Integer hamburguesa;
+
+    @Schema(example = "4") @NotNull(message = "El ID de la bebida es obligatorio")
+    @Min(value = 1, message = "El valor minimo para el ID es 1")
+    private Integer bebida;
 }
