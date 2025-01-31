5f72cbc028ac2bb84baa875736fef8574e102adc
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-30 21:33:02 -0300

    Documentado InsumoController e InsumoDTO

--- src/main/java/org/spdgroup/bigbitebackend/controllers/InsumoController.java
+++ src/main/java/org/spdgroup/bigbitebackend/controllers/InsumoController.java
@@ -1,5 +1,13 @@
 package org.spdgroup.bigbitebackend.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.spdgroup.bigbitebackend.model.dtos.InsumoDTO;
 import org.spdgroup.bigbitebackend.model.entities.Insumo;
 import org.spdgroup.bigbitebackend.services.InsumoService;
@@ -11,34 +19,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Insumos", description = "Metodos para registrar, obtener y editar insumos")
 @RequestMapping("/api/insumos")
 public class InsumoController {
 
     @Autowired
     private InsumoService insumoService;
 
-    @GetMapping
+    @Operation(summary = "Devuelve todos los insumos de la BBDD")
+    @GetMapping @ResponseBody
     public List<Insumo> obtenerInsumos() {
         return insumoService.obtenerInsumos();
     }
 
+    @Operation(summary = "Registra un nuevo insumo en la BBDD")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "201", description = "Insumo registrado", content = {
+                    @Content(mediaType = "application/json",
+                            schema =  @Schema(implementation = InsumoDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos", content = @Content)
+    })
     @PostMapping("/registrar")
-    public ResponseEntity<String> registrarInsumo(@RequestBody InsumoDTO insumoDTO) {
-        try {
-            insumoService.registrarInsumo(insumoDTO);
-            return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> registrarInsumo(@RequestBody @Valid InsumoDTO insumoDTO) {
+        insumoService.registrarInsumo(insumoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Registro exitoso");
     }
 
+    @Operation(summary = "Edita un insumo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Insumo editado", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = InsumoDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Insumo no encontrado", content = @Content)
+    })
     @PutMapping("/editar/{id}")
-    public ResponseEntity<String> editarInsumo(@RequestBody InsumoDTO insumoDTO, @PathVariable Long id) {
-        try {
-            insumoService.editarInsumo(insumoDTO, id);
-            return new ResponseEntity<>("Edición exitosa", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> editarInsumo(@RequestBody @Valid InsumoDTO insumoDTO,
+                                               @Parameter(description = "ID del insumo", example = "1") @PathVariable Long id) {
+        insumoService.editarInsumo(insumoDTO, id);
+        return ResponseEntity.status(HttpStatus.OK).body("Edicion exitosa");
     }
 }
--- src/main/java/org/spdgroup/bigbitebackend/model/dtos/InsumoDTO.java
+++ src/main/java/org/spdgroup/bigbitebackend/model/dtos/InsumoDTO.java
@@ -1,5 +1,9 @@
 package org.spdgroup.bigbitebackend.model.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,8 +12,17 @@ import lombok.*;
 @Getter @Setter
 public class InsumoDTO {
 
+    @Schema(example = "Pan para hamburguesas") @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
-    private double precio;
-    private double stock;
+
+    @Schema(example = "200", description = "(Precio de costo)") @NotNull(message = "El precio de costo (precio) es obligatorio")
+    @Min(value = 0, message = "El valor minimo del precio de costo (precio) es 0")
+    private Double precio;
+
+    @Schema(example = "600") @NotNull(message = "El stock es obligatorio")
+    @Min(value = 0, message = "El valor minimo del stock es 0")
+    private Integer stock;
+
+    @Schema(example = "UNIDADES") @NotBlank(message = "La unidad de medida es obligatoria")
     private String unidadMedida;
 }
