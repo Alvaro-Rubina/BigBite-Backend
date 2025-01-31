97b4a888e070d2f2c1500dc4442582d66ea40984
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-31 19:05:32 -0300

    Actualizado WebConfig

--- src/main/java/org/spdgroup/bigbitebackend/utils/WebConfig.java
+++ src/main/java/org/spdgroup/bigbitebackend/utils/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
