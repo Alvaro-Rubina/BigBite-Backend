c719ca6c671c86792f765b2f2658a5b9783b9a5d
Alvaro Rubina <alvarorubina132@gmail.com>
2025-01-31 19:05:22 -0300

    Actualizada info de la api en SwaggerConfig

--- src/main/java/org/spdgroup/bigbitebackend/utils/swagger/SwaggerConfig.java
+++ src/main/java/org/spdgroup/bigbitebackend/utils/swagger/SwaggerConfig.java
@@ -2,7 +2,6 @@ package org.spdgroup.bigbitebackend.utils.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +18,7 @@ public class SwaggerConfig {
                         .description("API desarrollada con Spring Boot conectada a una base de datos MySQL, " +
                                 "que al ser consumida por un cliente, en este caso un e-commerce de hamburguesas, " +
                                 "Big Bite, permite la gestion y persistencia de productos, pedidos y asientos contables.\n\n" +
+                                "Para probar la pagina en si y su interaccion con esta api, puedes hacerlo a traves del siguiente enlace: (Acá irá el link de la pag deployada) \n\n" +
                                 "Repositorios:\n" +
                                 "- Backend: [https://github.com/Alvaro-Rubina/BigBite-Backend](https://github.com/Alvaro-Rubina/BigBite-Backend)\n" +
                                 "- Frontend: [https://github.com/candelabp/Big-bite](https://github.com/candelabp/Big-bite)")
